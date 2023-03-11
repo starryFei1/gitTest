8209266a436ae70c5722ac695b493d6fabf14c0a
starryFei1 <127587588+starryFei1@users.noreply.github.com>
2023-03-11 20:21:18 +0800

    feilou

--- src/main/java/org/git/gitRun.java
+++ src/main/java/org/git/gitRun.java
@@ -8,5 +8,6 @@ public class gitRun {
         System.out.println("hello git4");
         System.out.println("hello master");
         System.out.println("hello hot-fix");
+         System.out.println("hello 拉去合并");
     }
 }
